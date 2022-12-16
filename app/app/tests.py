134622cc5af6acb013259eec0d21d27a290bcc1a
Moh Ariful <nextkoding@gmail.com>
2022-12-16 08:25:34 +0700

    Fixed .flake8 & tests.py

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,17 +5,18 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
 
-    def test_add_number(self):
+    def test_add_numbers(self):
         """Test adding numbers together"""
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        """Tes substracting numbers"""
+        """Test substracting numbers."""
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
