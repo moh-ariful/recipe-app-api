4227e82947d0a50d0c7d2793efe64d9281c12f53
Moh Ariful <nextkoding@gmail.com>
2022-12-18 12:17:59 +0700

    Implement user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,7 +9,7 @@ class ModelTests(TestCase):
     """Test models."""
 
     def test_create_user_with_email_successful(self):
-        """Test creating a user with an email is successful"""
+        """Test creating a user with an email is successful."""
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
