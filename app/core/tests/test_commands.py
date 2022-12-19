c61ad1ac7e577e7d446da648b16de447992dcb8c
Moh Ariful <nextkoding@gmail.com>
2022-12-19 14:32:11 +0700

    Fixed test_commands.py

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -22,13 +22,13 @@ class CommandTests(SimpleTestCase):
 
         patched_check.assert_called_once_with(databases=['default'])
 
-        @patch('time.sleep')
-        def test_wait_for_db_delay(self, patched_sleep, patched_check):
-            """Test waiting for database when getting OperationalError."""
-            patched_check.side_effect = [Psycopg2OpError] * 2 + \
-                [OperationalError] * 3 + [True]
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for database when getting OperationalError."""
+        patched_check.side_effect = [Psycopg2OpError] * 2 + \
+            [OperationalError] * 3 + [True]
 
-            call_command('wait_for_db')
+        call_command('wait_for_db')
 
-            self.assertEqual(patched_check.call_count, 6)
-            patched_check.assert_called_with(databases=['default'])
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
